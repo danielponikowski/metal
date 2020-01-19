20f21ceb345e479316b96ef2aea50fded64751b5
Marcin Seliga <marcin-sel@wp.pl>
2020-01-19 21:08:42 +0100

    Cos probowale, bez wiekszych efektow

--- scripts/uzupelnie_brakow_wila910.R
+++ scripts/uzupelnie_brakow_wila910.R
@@ -1,21 +1,31 @@
 library(dplyr)
+library(randomForest)
+library(gbm)
 
 dane_wola<-read.csv('./Github/metal/data/dane-wola-910obser.csv', 
                check.names=FALSE)
+load('./Github/metal/data/dane-zeliwo-uzupelnienie_tw.rda')
+
 dane_wola<-dane_wola[,-c(1,2)]
 dane_wola
 
 dane<-read.csv('./Github/metal/data/dane-zeliwo-uzupelnienie_tw.csv')
 
-library(randomForest)
+s<-sample(nrow(dane_wola), nrow(dane_wola)*0.33)
+dane_wola_train<-dane_wola[-s,]
+dane_wola_test<-dane_wola[s,]
 
-
-tward<-randomForest(tward~., data=dane_wola)
-
-bledy<-abs(predict(tward)-dane_wola$tward)/dane_wola$tward
+tward<-randomForest(tward~., data=dane_wola_train)
+bledy<-(predict(tward, dane_wola_test)-dane_wola_test$tward)/dane_wola_test$tward
 density(bledy)%>%plot()
 
 importance(tward)
+
+
+tward<-gbm(tward~., data=dane_wola_train, n.trees=1000)
+bledy<-(predict(tward, dane_wola_test, n.trees = 1000)-dane_wola_test$tward)/dane_wola_test$tward
+
+density(bledy)%>%plot()
 
 
 
@@ -33,16 +43,23 @@ dane$Cr....,
 dane$Ni....,
 dane$Cu....,
 dane$Mg....,
-dane$Rm..MPa.,
+dane$R0.2..MPa.,
 dane$A5....,
-dane$Twardość.Vickersa..HV.
+dane$Twardość.Brinella..HB.
 )
 colnames(dane_2)<-colnames(dane_wola)
 
 
-bledy_2<-abs(predict(tward, dane_2)-dane_2$tward)/dane_2$tward
-bledy_2 %>% na.omit() %>%density()%>%plot()
+
+bledy_2<-(predict(tward, dane_2, n.trees = 100)-dane_2$tward)/dane_2$tward
+bledy_2 %>% na.omit() %>%density()%>%plot(xlim=c(-2,2))
 
 
-dane_wola$tward%>% na.omit() %>%density()%>%plot()
-dane$Twardość.Vickersa..HV.%>% na.omit() %>%density()%>%plot()
+i<-1
+
+names(dane_2)[i]
+dane_2[,i][[1]]%>%na.omit()%>%density()%>%plot()
+dane_wola[,i]%>%na.omit()%>%density()%>%plot()
+i<-i+1
+
+i<-10
