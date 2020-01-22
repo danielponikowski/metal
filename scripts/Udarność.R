7bbe723e13eabbdbc7e3e8e1a9fef26ca9a5a25e
Marcin Seliga <marcin-sel@wp.pl>
2020-01-22 20:26:55 +0100

    Udarnosc

--- scripts/Udarność.R
+++ scripts/Udarność.R
@@ -99,6 +99,7 @@ dane_test_final<-dane_test_final[,braki_danych>q]
 
 m<-gbm(`Udarność Charpy [J]`~., data = dane_train, 
        n.trees =8000)
+y_min<-min(dane_train$`Udarność Charpy [J]`)
 
 
 y<-dane_test$`Udarność Charpy [J]`
@@ -111,9 +112,9 @@ for(i in seq_along(l)){
 plot(l, mse, type='l')
 (n_trees<-l[which.min(mse)])
 
-
+# Wychodziły czasami ujemne predykcje
 y<-dane_test$`Udarność Charpy [J]`
-y_pred<-predict(m, dane_test, n.trees = n_trees)
+y_pred<-pmax(y_min, predict(m, dane_test, n.trees = n_trees))
 
 res<-(y-y_pred)
 mean(res^2)
@@ -201,80 +202,80 @@ mean(res2^2)
 
 
 ########## Poniżej nieodświeżony, prawdopodobnie niedziałający kod. Nie uruchamiać.
-
-########## Lasy
-
-dane__<-dane_
-dane__$`Nr źródła`<-NULL # czepial sie tej zmiennej. Pewnie daltego, ze to factor
-names(dane__)<-make.names(names(dane__))
-dane_test<-dane__[s,]
-dane_train<-dane__[-s,]
-
-m<-randomForest(`Udarność.Charpy..J.`~., data = dane_train, 
-       n.trees = 100000)
-
-
-y_pred<-predict(m, dane_test)
-y<-dane_test$`Udarność.Charpy..J.`
-
-res<-(y-y_pred)
-mean(res^2)
-
-
-res%>%density()%>%plot()
-(res/y)%>%density()%>%plot()
-
-mean(abs((res/y))<0.2)
-
-o<-order(-abs(res))
-y[o]
-y_pred[o]
-
-
-plot(sort(y), ylab = 'Udarność')
-points(y_pred[order(y)], col = 'red')
-
-
-
-
-
-
-# Poniżej jakiś śmieciowy kod. Chcialem zrobic cross walidacje, ale nic madrego mi nie 
-# wyszlo. XGBoost nie chcial działać :(
-
-m<-gbm(`Udarność Charpy [J]`~., data = dane_, 
-       n.trees = 10000, cv.folds = 10, bag.fraction = 0.3)
-
-m<-gbm(`Udarność Charpy [J]`~., data = dane_,
-         family = "gaussian",
-         bag.fraction = 0.5)
-
-
-
-
-
-
-
-library(tidyverse)
-library(caret)
-library(xgboost)
-
-sum(is.na(as.matrix(dane__)))
-
-
-set.seed(123)
-dane__<-dane_
-dane__$`Nr źródła`<-NULL
-names(dane__)<-  make.names(names(dane__))
-
-model <- train(
-  Udział.austenitu.. ~., data = dane__, method = "xgbTree"
-  # ,
-  # trControl = trainControl("cv", number = 10)
-)
-# Best tuning parameter
-model$bestTune
-
-
-
-
+# 
+# ########## Lasy
+# 
+# dane__<-dane_
+# dane__$`Nr źródła`<-NULL # czepial sie tej zmiennej. Pewnie daltego, ze to factor
+# names(dane__)<-make.names(names(dane__))
+# dane_test<-dane__[s,]
+# dane_train<-dane__[-s,]
+# 
+# m<-randomForest(`Udarność.Charpy..J.`~., data = dane_train, 
+#        n.trees = 100000)
+# 
+# 
+# y_pred<-predict(m, dane_test)
+# y<-dane_test$`Udarność.Charpy..J.`
+# 
+# res<-(y-y_pred)
+# mean(res^2)
+# 
+# 
+# res%>%density()%>%plot()
+# (res/y)%>%density()%>%plot()
+# 
+# mean(abs((res/y))<0.2)
+# 
+# o<-order(-abs(res))
+# y[o]
+# y_pred[o]
+# 
+# 
+# plot(sort(y), ylab = 'Udarność')
+# points(y_pred[order(y)], col = 'red')
+# 
+# 
+# 
+# 
+# 
+# 
+# # Poniżej jakiś śmieciowy kod. Chcialem zrobic cross walidacje, ale nic madrego mi nie 
+# # wyszlo. XGBoost nie chcial działać :(
+# 
+# m<-gbm(`Udarność Charpy [J]`~., data = dane_, 
+#        n.trees = 10000, cv.folds = 10, bag.fraction = 0.3)
+# 
+# m<-gbm(`Udarność Charpy [J]`~., data = dane_,
+#          family = "gaussian",
+#          bag.fraction = 0.5)
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# library(tidyverse)
+# library(caret)
+# library(xgboost)
+# 
+# sum(is.na(as.matrix(dane__)))
+# 
+# 
+# set.seed(123)
+# dane__<-dane_
+# dane__$`Nr źródła`<-NULL
+# names(dane__)<-  make.names(names(dane__))
+# 
+# model <- train(
+#   Udział.austenitu.. ~., data = dane__, method = "xgbTree"
+#   # ,
+#   # trControl = trainControl("cv", number = 10)
+# )
+# # Best tuning parameter
+# model$bestTune
+# 
+# 
+# 
+# 
